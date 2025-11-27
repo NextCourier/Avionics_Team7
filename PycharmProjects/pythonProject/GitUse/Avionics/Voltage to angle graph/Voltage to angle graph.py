d92e586658884e495819c04745e4fb6b4ef84c79
benj2512 <benjerram25@gmail.com>
2025-11-27 14:07:15 +0000

    added simple voltage vs time graphing script

---
+++ PycharmProjects/pythonProject/GitUse/Avionics/Voltage to angle graph/Voltage to angle graph.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+voltage_data = pd.read_csv("Lua Script Voltage Log.csv") #read csv file
+
+time = voltage_data["Time [ms]"]
+sensor0 = voltage_data[" Sensor0 [mV]"]
+
+plt.figure(figsize = (10,10))   #plot voltage vs time graph
+plt.plot(time, sensor0, color = "red")
+plt.xlabel("Time [ms]")
+plt.ylabel("Voltage [mV]")
+plt.title("Voltage vs time for encoder")
+
+plt.savefig("Voltage-angle_graph.jpg",dpi=300, bbox_inches = "tight")   #save graph
+plt.show()
+
