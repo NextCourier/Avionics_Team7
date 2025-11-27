a5320587cd4d1da6c127b97590b09b99b810e198
benj2512 <benjerram25@gmail.com>
2025-11-27 14:19:01 +0000

    tweaked resolution

--- PycharmProjects/pythonProject/GitUse/Avionics/Voltage to angle graph/Voltage to angle graph.py
+++ PycharmProjects/pythonProject/GitUse/Avionics/Voltage to angle graph/Voltage to angle graph.py
@@ -6,7 +6,7 @@ voltage_data = pd.read_csv("Lua Script Voltage Log.csv") #read csv file
 time = voltage_data["Time [ms]"]
 sensor0 = voltage_data[" Sensor0 [mV]"]
 
-plt.figure(figsize = (10,10))   #plot voltage vs time graph
+plt.figure(figsize = (10,5))   #plot voltage vs time graph
 plt.plot(time, sensor0, color = "red")
 plt.xlabel("Time [ms]")
 plt.ylabel("Voltage [mV]")
